0ea8ecceebcd4220078faf7ca22ce8e9c6fc733b
Nikolas Soares <nikolassoares84@gmail.com>
2021-09-26 18:54:33 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM maven:3.8.2-jdk-8 as build-spring
+FROM maven:3.8.2-jdk-8 as build
 
 WORKDIR /api-rest-docker
 COPY . .
-RUN mvn clean install
+RUN mvn package -Dmaven.test.skip
 
 CMD mvn spring-boot:run
--- deployStack.sh
+++ deployStack.sh
@@ -4,8 +4,8 @@ echo "inicializa o cluster swarm"
 
 docker swarm init
 
-docker stack deploy -c docker-compose.yml build-spring
+docker stack deploy -c docker-compose.yml build
 
-docker stack services build-spring
+docker stack services build
 
-echo "build-spring rodando localhost:8080"
+echo "build rodando localhost:8080"

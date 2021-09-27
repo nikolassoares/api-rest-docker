d8e67bb618f615f6ae675283564a15b2bd113d62
Nikolas Soares <nikolassoares84@gmail.com>
2021-09-26 22:35:46 -0300

    Update deployStack.sh

--- deployStack.sh
+++ deployStack.sh
@@ -4,8 +4,8 @@ echo "inicializa o cluster swarm"
 
 docker swarm init
 
-docker stack deploy -c docker-compose.yml db-mysql-service
+docker stack deploy -c docker-compose.yml stack-service
 
-docker stack services db-mysql-service
+docker stack services stack-service
 
-echo "service-mysql-spring-boot rodando localhost:8080"
+echo "stack-service rodando localhost:8080"
